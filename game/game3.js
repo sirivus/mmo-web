831684ccc10f73b9c2d1153a7e2c282da513413e
siri <louishsu13@gmail.com>
2022-03-16 23:02:04 -0700

    setintervaled

--- game/game3.js
+++ game/game3.js
@@ -1,7 +1,7 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(
   75,
-  window.innerWidth / window.innerHeight,
+ (window.innerWidth - _winmargin )/ window.innerHeight,
   0.1,
   1000
 );
---
+++ game/resize.js
@@ -0,0 +1,9 @@
+function resize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener("resize", resize);
+
+//renderer();
