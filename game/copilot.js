7230352bc0449554da5b4b082797c3985b2a3d42
siri <louishsu13@gmail.com>
2022-03-26 06:34:12 -0700

    working tree works

--- game/copilot.js
+++ game/copilot.js
@@ -6,3 +6,25 @@ function countto10(){
     }
 
 }countto10();
+
+
+//count to 50
+function countto50(){
+    var i = 0;
+    while(i < 50){
+        i++;
+            console.log(i);
+    }
+
+}countto50();
+
+//draw a cube
+function drawCube(){
+    var i = 0;
+    while(i < 10){
+        i++;
+            console.log("*");
+    }
+
+}   drawCube();             
+
