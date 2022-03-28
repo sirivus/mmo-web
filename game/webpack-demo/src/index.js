5c8e93dab3375fa71ac44961ba35f104290873b7
siri <louishsu13@gmail.com>
2022-03-28 13:54:13 -0700

    prev commit, done fix

--- game/webpack-demo/src/index.js
+++ game/webpack-demo/src/index.js
@@ -1,15 +1,15 @@
 import _ from "lodash";
-//import Print from "./print.js";
+import Print from "./print.js";
 
 function component() {
   const element = document.createElement("div");
   const btn = document.createElement("button");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  //element.onclick = Print.bind(null, "Hello webpack!");
+  element.onclick = Print.bind(null, "Hello webpack!");
 
   btn.innerHTML = "Click me and check the console!";
-  btn.onclick = printMe;
+  btn.onclick = Print;
 
   element.appendChild(btn);
 
