2eb5dd11718d41dddfb5dc063e6f8605efe7edc2
siri <louishsu13@gmail.com>
2022-03-28 13:11:06 -0700

    webpacking

--- game/game3.js
+++ game/game3.js
@@ -1,69 +1,48 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(
   75,
- (window.innerWidth - _winmargin )/ window.innerHeight,
+  (window.innerWidth - _winmargin) / window.innerHeight,
   0.1,
-  1000
+  1e3
 );
-
 var renderer = new THREE.WebGLRenderer();
-//renderer.setSize(window.innerWidth, window.innerHeight);
-
 document.body.appendChild(renderer.domElement);
-
 var geometry = new THREE.BoxGeometry(20, 20, 20);
-var material = new THREE.MeshLambertMaterial({ color: 0xfd59d7 });
+var material = new THREE.MeshLambertMaterial({ color: 16603607 });
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
-
 camera.position.z = 100;
-
-var light = new THREE.PointLight(0xffff00);
+var light = new THREE.PointLight(16776960);
 light.position.set(10, 0, 25);
 scene.add(light);
-
 let clock = new THREE.Clock();
 let delta = 0;
-// _fps = 30
-//let interval = 1 / _fps;
-
 function update() {
-  var render = function () {
-    requestAnimationFrame(render);
-    //delta += clock.getDelta();
+  var e = function () {
+    requestAnimationFrame(e);
     renderer.setSize(window.innerWidth - _winmargin, window.innerHeight);
-    cube.rotation.x += .06;
-    cube.rotation.y += .06;
+    cube.rotation.x += 0.06;
+    cube.rotation.y += 0.06;
     camera.updateProjectionMatrix();
-    //if (delta > interval) {
-    //  // The draw or time dependent code are here
-    //  renderer.render(scene, camera);
-    //  delta = delta % interval;
-    //}
     renderer.render(scene, camera);
   };
-  render();
+  e();
 }
 update();
-
-// dat gui
 var gui = new dat.GUI();
 var cameraGui = gui.addFolder("camera position");
 cameraGui.add(camera.position, "x");
 cameraGui.add(camera.position, "y");
 cameraGui.add(camera.position, "z");
 cameraGui.open();
-
 var cameraGui = gui.addFolder("camera projection");
 cameraGui.add(camera, "fov");
 cameraGui.open();
-
 var lightGui = gui.addFolder("light position");
 lightGui.add(light.position, "x");
 lightGui.add(light.position, "y");
 lightGui.add(light.position, "z");
 lightGui.open();
-
 var cubeGui = gui.addFolder("cube position");
 cubeGui.add(cube.position, "x");
 cubeGui.add(cube.position, "y");
---
+++ game/practice/notes.js
@@ -0,0 +1,23 @@
+    //class or parent
+class asd {
+
+    //constructor is just another method
+  construct() {}
+    // methods
+  draw() {
+      //properties
+    c.drawImage(this.image, this.position.x, this.position.y);
+  }
+
+  update() {
+    this.draw();
+  }
+}
+// with classes, the code is much cleaner and easier to read. and it's easier to debug.
+// with classes, you can also use inheritance.
+// can define functions inside a class.
+// can use this.super() to call the parent class.
+// can use super.method() to call the parent class's method.
+// can use super.property to call the parent class's property.
+// can use super.property = value to set the parent class's property.
+
