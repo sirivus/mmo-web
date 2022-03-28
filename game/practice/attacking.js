f43a6ac593be008d636d58259b2bf3522db1604d
siri <louishsu13@gmail.com>
2022-03-27 23:35:45 -0700

    add attack

---
+++ game/practice/attacking.js
@@ -0,0 +1,15 @@
+//delaying
+function player(){
+ function attack () {
+    this.isAttacking = true
+    setTimeout(() => {
+        //code  
+        this.isAttacking = false
+    }, 100)
+}
+}
+//case ' ':
+    if (this.isAttacking) {
+player.attack
+//break;
+    }
