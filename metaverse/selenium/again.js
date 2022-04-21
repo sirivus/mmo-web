df80973989b0e3f3eea37fd31359b2c9281db098
Sirivus <louishsu13@gmail.com>
2022-04-20 18:52:00 -0700

    as

--- metaverse/selenium/again.js
+++ metaverse/selenium/again.js
@@ -1,5 +1,6 @@
 const {Builder} = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 
-const service = new chrome.ServiceBuilder('/path/to/chromedriver');
-const driver = new Builder().forBrowser('chrome').setChromeService(service).build();
+const service = new chrome.ServiceBuilder('/path/to/geckodriver');
+const driver = new Builder().forBrowser('firefox').setChromeService(service).build();
+
