a9388087fe6711225d1a426231633ae47b1d08e0
Sirivus <louishsu13@gmail.com>
2022-04-20 19:06:24 -0700

    trash requirejs

--- metaverse/selenium/again.js
+++ metaverse/selenium/again.js
@@ -1,6 +1,31 @@
 const {Builder} = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/chrome');
+const firefox = require('selenium-webdriver/firefox');
 
-const service = new chrome.ServiceBuilder('/path/to/geckodriver');
-const driver = new Builder().forBrowser('firefox').setChromeService(service).build();
+const service = new firefox.ServiceBuilder('/path/to/geckodriver');
+const driver = new Builder().forBrowser('firefox').setFirefoxService(service).build();
 
+require(['selenium-webdriver'], function *(Builder, By, Key, until ) {
+    //foo is now loaded.
+});
+        //require([selenium-webdriver], 
+        const { Builder, By, Key, until } = require('selenium-webdriver');
+
+        (async function helloSelenium() {
+            let driver = await new Builder().forBrowser('firefox').build();
+
+            await driver.get('https://www.google.com');
+
+            await driver.getTitle(); // => "Google"
+
+            driver.manage().setTimeouts({ implicit: 0.5 })
+
+            let searchBox = await driver.findElement(By.name('q'));
+            let searchButton = await driver.findElement(By.name('btnK'));
+
+            await searchBox.sendKeys('Selenium');
+            await searchButton.click();
+
+            await driver.findElement(By.name('q')).getAttribute("value"); // => 'Selenium'
+
+            await driver.quit();
+        })();
