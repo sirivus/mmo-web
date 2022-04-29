f3fff5110768b5cffd3423a03ed92d6dfeea78e2
Sirivus <louishsu13@gmail.com>
2022-04-29 03:29:32 -0700

    save

--- metaverse/browserify/dist/app-bundle.js
+++ metaverse/browserify/dist/app-bundle.js
@@ -1,13 +1,13 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./app/automation.tsx":
-/*!****************************!*\
-  !*** ./app/automation.tsx ***!
-  \****************************/
+/***/ "./app.tsx":
+/*!*****************!*\
+  !*** ./app.tsx ***!
+  \*****************/
 /***/ (() => {
 
-throw new Error("Module build failed (from ./node_modules/ts-loader/index.js):\nError: TypeScript emitted no output for /Users/lhlh/mmo-web/metaverse/browserify/app/automation.tsx.\n    at makeSourceMapAndFinish (/Users/lhlh/mmo-web/metaverse/browserify/node_modules/ts-loader/dist/index.js:52:18)\n    at successLoader (/Users/lhlh/mmo-web/metaverse/browserify/node_modules/ts-loader/dist/index.js:39:5)\n    at Object.loader (/Users/lhlh/mmo-web/metaverse/browserify/node_modules/ts-loader/dist/index.js:22:5)");
+throw new Error("Module build failed (from ./node_modules/ts-loader/index.js):\nError: TypeScript emitted no output for /Users/lhlh/mmo-web/metaverse/browserify/app.tsx.\n    at makeSourceMapAndFinish (/Users/lhlh/mmo-web/metaverse/browserify/node_modules/ts-loader/dist/index.js:52:18)\n    at successLoader (/Users/lhlh/mmo-web/metaverse/browserify/node_modules/ts-loader/dist/index.js:39:5)\n    at Object.loader (/Users/lhlh/mmo-web/metaverse/browserify/node_modules/ts-loader/dist/index.js:22:5)");
 
 /***/ })
 
@@ -18,7 +18,7 @@ throw new Error("Module build failed (from ./node_modules/ts-loader/index.js):\n
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
 /******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./app/automation.tsx"]();
+/******/ 	__webpack_modules__["./app.tsx"]();
 /******/ 	
 /******/ })()
 ;
