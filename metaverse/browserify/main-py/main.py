bd0b19769cfbb92f6c4bdba8cfa6ce8b2570152f
Sirivus <louishsu13@gmail.com>
2022-05-01 00:06:26 -0700

    save

---
+++ metaverse/browserify/main-py/main.py
@@ -0,0 +1,9 @@
+# main.py
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch()
+    page = browser.new_page()
+    page.goto("http://whatsmyuseragent.org/")
+    page.screenshot(path="example.png")
+    browser.close()
