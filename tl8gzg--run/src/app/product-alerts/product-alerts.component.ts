3a00751fd0befdde57753a8dc19e22bf450d060e
Sirivus <louishsu13@gmail.com>
2022-04-30 23:00:25 -0700

    save

--- tl8gzg--run/src/app/product-alerts/product-alerts.component.ts
+++ tl8gzg--run/src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../products';
 
 @Component({
   selector: 'app-product-alerts',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductAlertsComponent implements OnInit {
 
+  @Input() product!: Product;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
