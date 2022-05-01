f783625c798062f86d8982d2a616c5969be9f7aa
Sirivus <louishsu13@gmail.com>
2022-04-30 17:45:26 -0700

    save

---
+++ metaverse/browserify/browser/browser.js
@@ -0,0 +1,14 @@
+const { chromium } = require('playwright');  // Or 'firefox' or 'webkit'.
+
+(async () => {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+  await page.goto('https://example.com');
+  // other actions...
+  await browser.close();
+})();
+
+//import ignore from 'electron/index.js';
+//ignore();
+
+//module.exports = {"browser": true}
