30ea14fd265736221780b7901c3474462039c8b2
Sirivus <louishsu13@gmail.com>
2022-05-12 19:13:14 -0700

    save

---
+++ script.js
@@ -0,0 +1,12 @@
+//browserify script.js -t babelify --outfile bundle.js
+
+var fs = require("fs");
+var browserify = require("browserify");
+var babelify = require("babelify");
+
+browserify({ debug: true })
+  .transform(babelify)
+  .require("./script.js", { entry: true })
+  .bundle()
+  .on("error", function (err) { console.log("Error: " + err.message); })
+  .pipe(fs.createWriteStream("bundle.js"));
