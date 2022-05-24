957afbb17022619e72d24d7809e5fc8aa19d4860
Sirivus <louishsu13@gmail.com>
2022-05-24 07:03:40 -0700

    save

--- metaverse/node.js
+++ metaverse/node.js
@@ -1,15 +1,14 @@
-
 const { chromium } = require('playwright');
-
 (async () => {
   const browser = await chromium.launch({
     headless: false
   });
   const context = await browser.newContext();
+  // Open new page
+  const page = await context.newPage();
   // Go to https://app.meliopayments.com/meliome/pay/metaverse
   await page.goto('https://app.meliopayments.com/meliome/pay/metaverse');
   // ---------------------
   await context.close();
   await browser.close();
-  node;
 })();
